a8781faaf8fe52a4907c0ee2291b083bb6956e7a
Harini-04 <harinikurella2003@gmail.com>
2025-11-08 22:06:08 +0530

    Updated About Me and background colors

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,8 +6,9 @@ function About() {
     <section id="about" className="about">
       <h2>About Me</h2>
       <p>
-        I'm a Frontend Developer with a strong interest in React, JavaScript, and UI design.
-        I love creating modern, accessible, and responsive web apps that look great on all devices.
+        A passionate and curious learner, I constantly strive to expand my knowledge across Software Development, Artificial Intelligence, and Machine Learning. 
+        With hands-on experience in Apex, LWC, and data-driven projects, I aim to create impactful and efficient solutions. I'm eager to contribute my skills to 
+        a reputed organization, collaborate with talented professionals, and continue growing on a path of innovation and excellence.
       </p>
     </section>
   );
