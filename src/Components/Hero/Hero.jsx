821f1f04191c2f75c8445caff89b4b51c56d7095
Harini-04 <harinikurella2003@gmail.com>
2025-11-08 22:10:34 +0530

    updated social icons and css

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,13 +3,33 @@ import "./Hero.css";
 import profile from "../../assets/profile.jpg"; 
 
 function Hero() {
+  const handleResume=()=>{
+    window.open("https://drive.google.com/file/d/1uB0rdepNV8C3p-l4NHYDh-I1tDrxSg0e/view?usp=drive_link","_blank");
+  };
   return (
     <section id="hero" className="hero">
       <div className="hero-content">
-        <h1>Hi, I'm Harini</h1>
-        <p>A passionate Frontend Developer crafting beautiful web experiences.</p>
-        <button>My Work</button>
+        <h3>Hi, I'm</h3>
+        <h1>Harini Kurella</h1>
+        <p>CSE 2024</p>
+        <button className="my-work" onClick={()=>{
+          const section=document.getElementById("work");
+          section?.scrollIntoView({behavior:"smooth"});
+        }}>My Work</button>
+        <button className="resume-btn" onClick={handleResume}>My Resume</button>
+        <div className="hero-icons">
+        <a href="https://www.linkedin.com/in/harini-kurella-039201245/" target="_blank" rel="noopener noreferrer">
+          <img src="./linkedin.png" alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/Harini-04" target="_blank" rel="noopener noreferrer">
+          <img src="./github.png" alt="GitHub" />
+        </a>
+        <a href="https://leetcode.com/Harini_kurella" target="_blank" rel="noopener noreferrer">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png" alt="LeetCode" />
+        </a>
+        </div>
       </div>
+      
       <img src={profile} alt="profile" className="hero-img" />
     </section>
   );
