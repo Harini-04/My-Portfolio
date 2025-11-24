8205b0baecd914492f6fd1ff7cc2fa2e643567b1
Harini-04 <harinikurella2003@gmail.com>
2025-11-24 11:18:40 +0530

    updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">My portfolio</div>
+      <div className="logo">Harini Kurella</div>
       <ul className="nav-links">
         <li><a href="#hero">Home</a></li>
         <li><a href="#about">About</a></li>
